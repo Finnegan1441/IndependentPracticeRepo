1bb862307e75b58e4a418cd33def32fc82a91a54
Finnegan1441 <patrickcummins2009@gmail.com>
2016-07-25 11:52:47 -0700

    Finished Adapter, populated Actor List in Main Activity, created and set adapter

--- IndependentPracticeRepoSharingApp/app/src/main/java/gg/patrickcummins/independentpracticereposharingapp/MainActivity.java
+++ IndependentPracticeRepoSharingApp/app/src/main/java/gg/patrickcummins/independentpracticereposharingapp/MainActivity.java
@@ -4,8 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     ListView mListView;
+    ArrayList actorList;
+    CustomApadter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +18,16 @@ public class MainActivity extends AppCompatActivity {
 
 
         setViews();
-        CustomApadter myAdapter;
+        populateList();
+        myAdapter = new CustomApadter(actorList, MainActivity.this);
 
-        mListView.setAdapter();
+        mListView.setAdapter(myAdapter);
+    }
+
+    private void populateList() {
+        actorList.add(new Actor("Leonardo Dicaprio", "November 11, 1974", 1));
+        actorList.add(new Actor("Matt Damon", "October 8, 1970", 1));
+        actorList.add(new Actor("Jennifer Lawrence", "August 15, 1990", 1));
     }
 
     private void setViews() {
